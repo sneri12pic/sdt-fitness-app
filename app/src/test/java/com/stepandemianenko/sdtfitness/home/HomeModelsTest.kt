2102fdab486879fab7c04cd5eaa3519e64a4852a
Ypo <stepan.demyanenko30@gmail.com>
2026-05-25 22:03:53 +0100

    Add Android unit tests and CI test step

---
+++ app/src/test/java/com/stepandemianenko/sdtfitness/home/HomeModelsTest.kt
@@ -0,0 +1,91 @@
+package com.stepandemianenko.sdtfitness.home
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.LocalDate
+
+class HomeModelsTest {
+
+    @Test
+    fun dailyQuestProgress_returnsFractionOfTarget() {
+        val quest = DailyQuestState(
+            targetSteps = 10_000,
+            currentSteps = 2_500
+        )
+
+        assertEquals(0.25f, quest.progress, 0.001f)
+    }
+
+    @Test
+    fun dailyQuestProgress_capsAtComplete() {
+        val quest = DailyQuestState(
+            targetSteps = 5_000,
+            currentSteps = 7_500
+        )
+
+        assertEquals(1f, quest.progress, 0.001f)
+    }
+
+    @Test
+    fun dailyQuestProgress_returnsZeroWhenTargetIsInvalid() {
+        val quest = DailyQuestState(
+            targetSteps = 0,
+            currentSteps = 2_000
+        )
+
+        assertEquals(0f, quest.progress, 0.001f)
+    }
+
+    @Test
+    fun dailyGoalSummaryOverallProgress_averagesStepsWorkoutsAndActiveMinutes() {
+        val summary = DailyGoalSummaryState(
+            stepsCurrent = 2_500,
+            stepsTarget = 5_000,
+            workoutsCompleted = 1,
+            workoutsTarget = 1,
+            activeMinutesCurrent = 15,
+            activeMinutesTarget = 30
+        )
+
+        assertEquals(0.666f, summary.overallProgress, 0.001f)
+    }
+
+    @Test
+    fun dailyGoalSummaryOverallProgress_capsWorkoutCompletion() {
+        val summary = DailyGoalSummaryState(
+            stepsCurrent = 5_000,
+            stepsTarget = 5_000,
+            workoutsCompleted = 3,
+            workoutsTarget = 1,
+            activeMinutesCurrent = 30,
+            activeMinutesTarget = 30
+        )
+
+        assertEquals(1f, summary.workoutsCompletedCapped.toFloat(), 0.001f)
+        assertEquals(1f, summary.overallProgress, 0.001f)
+    }
+
+    @Test
+    fun calculateCurrentStreak_countsConsecutiveDatesEndingToday() {
+        val today = LocalDate.of(2026, 5, 25)
+        val streakDates = setOf(
+            today,
+            today.minusDays(1),
+            today.minusDays(2),
+            today.minusDays(5)
+        )
+
+        assertEquals(3, calculateCurrentStreak(streakDates, today))
+    }
+
+    @Test
+    fun calculateCurrentStreak_returnsZeroWhenTodayIsMissing() {
+        val today = LocalDate.of(2026, 5, 25)
+        val streakDates = setOf(
+            today.minusDays(1),
+            today.minusDays(2)
+        )
+
+        assertEquals(0, calculateCurrentStreak(streakDates, today))
+    }
+}
